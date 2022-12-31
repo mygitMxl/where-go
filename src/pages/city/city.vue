42f84411094b222784bbb715b8511e64411db58c
mygitMxl <1065617571@qq.com>
2022-12-31 11:03:55 +0800

    SearchInput

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,14 +1,16 @@
 <template>
 <div>
     <Header></Header>
+    <Search></Search>
 </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Search from './components/citySearch.vue'
 export default {
 name:'v-city',
-components:{Header}
+components:{Header,Search}
 }
 </script>
 
---
+++ src/pages/city/components/citySearch.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input  class="searchInput" placeholder="请输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+name:'v-search'
+}
+</script>
+
+<style scoped>
+.search{
+    line-height: .72rem;
+    background-color:#00bcd4 ;
+    padding: 0 .1rem;
+}
+ .searchInput{
+    box-sizing: border-box;/* 加了box-sizing:border-box属性，padding和border的值就不会在影响元素的宽高 */
+     width: 100%;
+     text-align: center;
+     border-radius: .06rem;
+     height: .62rem;
+     color: #666;
+     padding: 0 .3rem;
+ }
+
+</style>
